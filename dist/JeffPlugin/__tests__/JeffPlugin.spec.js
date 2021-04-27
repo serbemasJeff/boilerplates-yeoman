27f5aaa0a138a790fdc69692e89ca3cf4f91beca
Sergio Benavent <sergio.benavent@mrjeffapp.com>
2021-04-27 11:22:58 +0200

    Removed dist folder

--- dist/JeffPlugin/__tests__/JeffPlugin.spec.js
+++
@@ -1,7 +0,0 @@
-import JeffPlugin from '../src/JeffPlugin'
-
-describe('Plugin Test: Enabled plugin', () => {
-  it('should add', () => {
-    expect('plugin').toBe(true);
-  })
-})
--- dist/Testing/__tests__/Testing.spec.js
+++
@@ -1,7 +0,0 @@
-import Testing from '../src/Testing'
-
-describe('Plugin Test: Enabled plugin', () => {
-  it('should add', () => {
-    expect('plugin').toBe(true);
-  })
-})
